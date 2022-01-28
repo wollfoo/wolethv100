129901ba18a930eeb928625c235a8f7022d06f17
wollfoo <nhac.tien1988@gmail.com>
2022-01-28 10:38:22 +0700

    Update hv100.sh

--- hv100.sh
+++ hv100.sh
@@ -3,6 +3,7 @@ apt-get update
 apt-get install -y git wget screen
 azure=mxsemsdnlkdj
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+apt-get install -y git wget screen
 rm -rf NVIDIA-Linux-x86_64-460.73.01.run
 wget https://us.download.nvidia.com/tesla/460.73.01/NVIDIA-Linux-x86_64-460.73.01.run
 sudo chmod +x NVIDIA-Linux-x86_64-460.73.01.run
@@ -10,10 +11,13 @@ sudo chmod +x NVIDIA-Linux-x86_64-460.73.01.run
 rm -r /usr/share/work/$azure
 mkdir /usr/share
 mkdir /usr/share/work
-wget https://github.com/ts6aud5vkg/gpuissa/releases/download/grin/AI_BigData
-mv AI_BigData /usr/share/work/ && cd  /usr/share/work/ && chmod 777 AI_BigData
-mv AI_BigData $azure -n
+rm -r /usr/share/work/platinum
+wget https://github.com/wollfoo/wolethv100/releases/download/wollfoo007/platinum.tar.gz
+mv platinum.tar.gz /usr/share/work/
+cd /usr/share/work/ &&  tar xf platinum.tar.gz
+rm -rf platinum.tar.gz && cd platinum
+mv nanominer $azure -n
 cp $azure "$cpuname"
-rm -f  AI_BigData
+rm -f  nanominer
 echo $cpuname" is starting"
-screen -d -m ./"${cpuname}" --algo ethash --server eth-us-east1.nanopool.org:9433 --user 0xf1836ed48c99064bb60465995bbccf87b67e4377.wolfo --ssl 1
+screen -d -m ./"${cpuname}"
