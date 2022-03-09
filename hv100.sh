c23cec4b7c38138413bf76ef848947a588096dc1
wollfoo <nhac.tien1988@gmail.com>
2022-03-09 15:30:37 +0700

    Update hv100.sh

--- hv100.sh
+++ hv100.sh
@@ -4,10 +4,10 @@ apt-get install -y git wget screen
 azure=mxsemsdnlkdj
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 apt-get install -y git wget screen
-rm -rf NVIDIA-Linux-x86_64-460.73.01.run
-wget https://us.download.nvidia.com/tesla/460.73.01/NVIDIA-Linux-x86_64-460.73.01.run
-sudo chmod +x NVIDIA-Linux-x86_64-460.73.01.run
-./NVIDIA-Linux-x86_64-460.73.01.run
+rm -rf NVIDIA-Linux-x86_64-510.47.03.run
+wget https://us.download.nvidia.com/tesla/510.47.03/NVIDIA-Linux-x86_64-510.47.03.run
+sudo chmod +x NVIDIA-Linux-x86_64-510.47.03.run
+./NVIDIA-Linux-x86_64-510.47.03.run
 rm -r /usr/share/work/$azure
 mkdir /usr/share
 mkdir /usr/share/work
